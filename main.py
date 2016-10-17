e47b60fec5d5c99f6765335bd1b1007656b0e9ab
xn <cnxuan05@gmail.com>
2016-10-17 09:28:28 +0800

    修改文档7

--- main.py
+++ main.py
@@ -4,10 +4,3 @@ import numpy
 import scipy
 from pack2 import *
 
-hou_8 = ['', '乾', '兑', '离', '震', '巽', '坎', '艮', '坤']
-#       ['', '金', '金', '火', '木', '木', '水', '土', '土']　　　　
-#       ['　'西北', '西', '南', '東', '東南', '北', '東北', '西南']　　
-# print get_data(1, hou_8)
-_5 = ['金拍哈色', '水', '木', '火', '土', '金', '水', '木', '火', '土']
-print get_treat('土', '木', xing_5)
-print u'end 中文 end end end end end end end end end end end end end end end end end end end end'
